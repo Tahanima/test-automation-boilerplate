4b00aca77d85ebeff6298428e0f4d814c5c2c8b8
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2020-07-27 20:45:53 +0600

    fixed priority val, added report and screenshot folders

--- src/test/java/automation/login/LoginTest.java
+++ src/test/java/automation/login/LoginTest.java
@@ -3,6 +3,7 @@ package automation.login;
 import automation.BaseTest;
 import com.github.javafaker.Faker;
 import org.testng.annotations.Test;
+import util.ScreenshotUtils;
 
 import static util.Globals.CONTEXT_URL;
 import static util.Globals.PRODUCT_PAGE_URL;
@@ -20,13 +21,15 @@ public class LoginTest extends BaseTest {
     @Test(description = "Testing Successful Login Attempt", priority = 0)
     public void testSuccessfulLogin() {
         login("standard_user", "secret_sauce");
+        ScreenshotUtils.captureFullPage(driver, "successfulLoginAttempt");
 
         assertThat(loginPage.getPageUrl().contentEquals(CONTEXT_URL + PRODUCT_PAGE_URL)).isTrue();
     }
 
-    @Test(description = "Testing Unsuccessful Login Attempt", priority = 0)
+    @Test(description = "Testing Unsuccessful Login Attempt", priority = 1)
     public void testUnsuccessfulLogin() {
         login(faker.name().username(), faker.internet().password());
+        ScreenshotUtils.captureFullPage(driver, "unsuccessfulLoginAttempt");
 
         assertThat(loginPage.getPageUrl().contentEquals(CONTEXT_URL + PRODUCT_PAGE_URL)).isFalse();
     }
