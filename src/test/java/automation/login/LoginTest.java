712319f8878e171d99d6552a3d1e3f59d36b0502
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2021-03-19 01:58:52 +0600

    updated code

--- src/test/java/automation/login/LoginTest.java
+++ src/test/java/automation/login/LoginTest.java
@@ -3,7 +3,6 @@ package automation.login;
 import automation.BaseTest;
 import com.github.javafaker.Faker;
 import org.testng.annotations.Test;
-import utils.ScreenshotUtils;
 
 import static utils.UrlConstants.BASE_URL;
 import static utils.UrlConstants.PRODUCT_PAGE_URL;
@@ -25,7 +24,6 @@ public class LoginTest extends BaseTest {
     @Test(description = "Testing Successful Login Attempt", priority = 0)
     public void testSuccessfulLogin() {
         login("standard_user", "secret_sauce");
-        ScreenshotUtils.captureFullPage(driver, "successfulLoginAttempt");
 
         assertThat(loginPage.getPageUrl()
                 .equals(BASE_URL + PRODUCT_PAGE_URL))
@@ -35,7 +33,6 @@ public class LoginTest extends BaseTest {
     @Test(description = "Testing Unsuccessful Login Attempt", priority = 1)
     public void testUnsuccessfulLogin() {
         login(faker.name().username(), faker.internet().password());
-        ScreenshotUtils.captureFullPage(driver, "unsuccessfulLoginAttempt");
 
         assertThat(loginPage.getPageUrl()
                 .equals(BASE_URL + PRODUCT_PAGE_URL))
